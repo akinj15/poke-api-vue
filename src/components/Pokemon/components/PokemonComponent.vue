b217023d257810f565c62500edad6212538fb67a
akinj15 <akin.bezerra123@gmail.com>
2022-07-28 22:39:21 +0000

    att

--- src/components/Pokemon/components/PokemonComponent.vue
+++ src/components/Pokemon/components/PokemonComponent.vue
@@ -1,8 +1,12 @@
 <template>
-    <h2 v-if="pokemonStore.pokemon">{{pokemonStore.pokemon.name}}</h2>
-    <img v-if="pokemonStore.pokemon" :src="pokemonStore.pokemon.sprites.other.dream_world.front_default" alt="">
-    <div  @click="voltar">
-        <button @click="voltar">voltar</button>
+    <div class="container">
+        <div class="pokemon">
+            <h2 v-if="pokemonStore.pokemon">{{pokemonStore.pokemon.name}}</h2>
+            <img v-if="pokemonStore.pokemon" :src="pokemonStore.pokemon.sprites.other.dream_world.front_default" alt="">
+        </div> 
+        <div>
+            <button @click="voltar">voltar</button>
+        </div>
     </div>
 </template>
 <script>
--- src/components/Pokemon/components/PokemonsComponent.vue
+++ src/components/Pokemon/components/PokemonsComponent.vue
@@ -1,12 +1,20 @@
 <template>
-    <h2>lista de pokemons</h2>
-    <div v-for="item in pokemonStore.list_pokemon" :key="item.name" >
-        <div @click="carregarPokemon(item)">
-             {{ item.name }} 
+    <div class="container">
+        <header>
+            <h2>lista de pokemons</h2>
+        </header>
+        <div class="pokemons">
+            <div v-for="item in pokemonStore.list_pokemon" :key="item.name" >
+                <div @click="carregarPokemon(item)">
+                     {{ item.name }} 
+                </div>
+            </div>
+        </div>
+        <div class="buttons">
+            <button @click="previous()">voltar</button>
+            <button @click="next()">next</button>
         </div>
     </div>
-        <button @click="previous()">voltar</button>
-        <button @click="next()">next</button>
 </template>
 <script>
     import Pokemons from "../controllers/PokemonsController.js";
